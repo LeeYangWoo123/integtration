d723dfcea57b692f953ba70750ebba1894766beb
이양우 <katarusis20188@gmail.com>
2026-03-04 14:49:14 +0900

    running

--- run_dashboard.bat
+++ run_dashboard.bat
@@ -1,3 +1,9 @@
 @echo off
+title Smart Equation Analyzer Master Launcher
+echo [1/2] Verifying installation of required libraries and graph engines...
+python -m pip install streamlit sympy numpy matplotlib
+
+echo.
+echo [2/2] Run the program...
 python -m streamlit run app.py
 pause
